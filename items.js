6c6dca1f63be319b915dfdb425e234a8d8d0d4ac
fahad khan <fahadkhan09922@gmail.com>
2023-06-22 04:29:12 +0500

    upsa

--- items.js
+++ items.js
@@ -1,82 +1,81 @@
-let items = [
-    {
-        id:1,
-        lesson:"Art And Craft",
-        location:"London",
-        price:22,
-        spaces:5,
-        image:"assets/art_and_craft.jpg"
+let items = [{
+        id: 1,
+        lesson: "Art And Craft",
+        location: "London",
+        price: 22,
+        spaces: 5,
+        image: "assets/art_and_craft.avif"
     },
     {
-        id:2,
-        lesson:"Sport",
-        location:"Oxford",
-        price:19,
-        spaces:5,
-        image:"assets/sport.jpg"
+        id: 2,
+        lesson: "Sport",
+        location: "Oxford",
+        price: 19,
+        spaces: 5,
+        image: "assets/sport.jpg"
     },
     {
-        id:3,
-        lesson:"Music",
-        location:"London",
-        price:20,
-        spaces:5,
-        image:"assets/music.jpg"
+        id: 3,
+        lesson: "Music",
+        location: "London",
+        price: 20,
+        spaces: 5,
+        image: "assets/music.jpg"
     },
     {
-        id:4,
-        lesson:"Theater",
-        location:"York",
-        price:17,
-        spaces:5,
-        image:"assets/theater.jpg"
+        id: 4,
+        lesson: "Theater",
+        location: "York",
+        price: 17,
+        spaces: 5,
+        image: "assets/theater.jpg"
     },
     {
-        id:5,
-        lesson:"Coding And Programming",
-        location:"Bristol",
-        price:7,
-        spaces:5,
-        image:"assets/coding.jpg"
+        id: 5,
+        lesson: "Coding And Programming",
+        location: "Bristol",
+        price: 7,
+        spaces: 5,
+        image: "assets/coding.webp"
     },
     {
-        id:6,
-        lesson:"Cooking And Baking",
-        location:"London",
-        price:8,
-        spaces:5,
-        image:"assets/cooking.jpg"
+        id: 6,
+        lesson: "Cooking And Baking",
+        location: "London",
+        price: 8,
+        spaces: 5,
+        image: "assets/cooking.avif"
     },
     {
-        id:7,
-        lesson:"Religous Activities",
-        location:"Manchester",
-        price:12,
-        spaces:5,
-        image:"assets/religious.jpg"
+        id: 7,
+        lesson: "Religous Activities",
+        location: "Manchester",
+        price: 12,
+        spaces: 5,
+        image: "assets/religious.webp"
     },
     {
-        id:8,
-        lesson:"Community Services",
-        location:"Liverpool",
-        price:9,
-        spaces:5,
-        image:"assets/community.jpg"
+        id: 8,
+        lesson: "Community Services",
+        location: "Liverpool",
+        price: 9,
+        spaces: 5,
+        image: "assets/community.jpg"
     },
     {
-        id:9,
-        lesson:"Theater",
-        location:"London",
-        price:13,
-        spaces:5,
-        image:"assets/theater.jpg"
+        id: 9,
+        lesson: "Theater",
+        location: "London",
+        price: 13,
+        spaces: 5,
+        image: "assets/theater.jpg"
     },
     {
-        id:10,
-        lesson:"Music",
-        location:"Chelsey",
-        price:16,
-        spaces:5,
-        image:"assets/music.jpg"
+        id: 10,
+        lesson: "Music",
+        location: "Chelsey",
+        price: 16,
+        spaces: 5,
+        image: "assets/music.jpg"
     }
 ]
